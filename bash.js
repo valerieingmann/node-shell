67197867ed6e0f8073511c843905b590bc8de01e
reenatt-dotcom <64491890+reenatt-dotcom@users.noreply.github.com>
2020-08-07 12:17:50 -0700

    Update bash.js

--- bash.js
+++ bash.js
@@ -1,1 +1,9 @@
 
+// we can access the pwd function and store it in a variable like so
+const pwd = require('./pwd');
+const fs = require('fs')
+const ls = require('./ls')
+// if a user enters 'pwd' as an argument, we can then call it
+pwd()
+console.log(ls)
+ls()
