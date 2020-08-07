9d5ea31051a6d556d38ab1e17d6824a813bd3fd7
reenatt-dotcom <64491890+reenatt-dotcom@users.noreply.github.com>
2020-08-07 12:19:09 -0700

    Create ls.js

---
+++ ls.js
@@ -0,0 +1,13 @@
+
+
+const ls = ()=>{fs.readdir('./', 'utf8', (err, files) => {
+    if (err){
+        throw err
+    }else{
+        process.stdout.write(files.join('\n'))
+        process.stdout.write("prompt > ")
+    }
+})
+}
+module.exports = ls
+console.log(ls)
