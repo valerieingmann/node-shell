ddb8324cc012ce1723f84dc4b34f533133dbd127
reenatt-dotcom <64491890+reenatt-dotcom@users.noreply.github.com>
2020-08-07 12:19:37 -0700

    Create pwd.js

---
+++ pwd.js
@@ -0,0 +1,15 @@
+module.exports = function () {
+    // pwd code
+    process.stdout.write('prompt > ')
+
+    process.stdin.on('data', (data) => {
+        
+        const cmd = data.toString().trim()
+        if (cmd==='pwd') process.stdout.write(process.cwd())
+        else process.stdout.write('You typed: ' + cmd)
+        
+        process.stdout.write('\nprompt > ')
+        
+    })
+    
+  }
